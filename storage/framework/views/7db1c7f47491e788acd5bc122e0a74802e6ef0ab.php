dc1625b2e516f82da28fe41cd7765513f89c599c
Kartib B <work.kartikb@gmail.com>
2023-09-27 14:36:49 +0530

    cache

---
+++ storage/framework/views/7db1c7f47491e788acd5bc122e0a74802e6ef0ab.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="bravo-text <?php echo e($class ?? ''); ?>">
+        <?php echo clean($content ?? ''); ?>
+
+    </div>
+</div><?php /**PATH C:\xampp\htdocs\torgi\themes/Base/Template/Views/frontend/blocks/text.blade.php ENDPATH**/ ?>
