93a3f28a222a6bd78b5afd7df11d3709ad05da1d
Snehali <snehaliburade@gmail.com>
2023-10-06 12:21:48 +0530

    Masnsger Dashboard

--- modules/Layout/parts/topbar.blade.php
+++ modules/Layout/parts/topbar.blade.php
@@ -33,7 +33,7 @@
                                     </li>
                                 @endif
                                 @if(is_vendor())
-                                <li class="menu-hr"><a href="{{route('vendor.dashboard')}}" class="menu-hr"><i class="icon ion-md-analytics"></i> {{__("Vendor Dashboard")}}</a></li>
+                                <li class="menu-hr"><a href="{{route('vendor.dashboard')}}" class="menu-hr"><i class="icon ion-md-analytics"></i> {{__("Dashboard")}}</a></li>
                                 @endif
                                 <li class="@if(is_vendor()) menu-hr @endif">
                                     <a href="{{route('user.profile.index')}}"><i class="icon ion-md-construct"></i> {{__("My profile")}}</a>
